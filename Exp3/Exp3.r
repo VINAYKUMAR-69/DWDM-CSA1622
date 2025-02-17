adfb1567b94e05adccefe02d85e837d9e0a60d10
VINAYKUMAR-69 <vinaykumarganga19@gmail.com>
2025-02-17 14:18:03 +0530

    Create Exp3.r

---
+++ Exp3/Exp3.r
@@ -0,0 +1,16 @@
+# Given data
+data <- c(200, 300, 400, 600, 1000)
+
+# (a) Min-Max Normalization (0 to 1)
+min_val <- min(data)
+max_val <- max(data)
+min_max_norm <- (data - min_val) / (max_val - min_val)
+
+# (b) Z-Score Normalization
+mean_val <- mean(data)
+sd_val <- sd(data)
+z_score_norm <- (data - mean_val) / sd_val
+print("Min-Max Normalization (0 to 1):")
+print(min_max_norm)
+print("Z-Score Normalization:")
+print(z_score_norm)
